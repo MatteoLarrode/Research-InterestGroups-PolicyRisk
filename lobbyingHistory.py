e85b2734ccd0661fdc8039623678d45a268d8fb2
MatteoLarrode <matteo.larrode@orange.fr>
2022-11-07 19:19:23 -0500

    improve data collection

--- lobbyingHistory.py
+++ lobbyingHistory.py
@@ -6,37 +6,43 @@ from config import api_key
 
 lobbying_history = {}
 
-subtopic = "biofuels"
+subtopic = "electric vehicles"
+subtopic_edited = "electric_vehicles"
 url = "https://lda.senate.gov/api/v1/filings/?filing_dt_posted_after=%s&filing_dt_posted_before=%s&filing_specific_lobbying_issues=%s" %("2011-01-01", "2021-12-31", subtopic)
 headers= {"Authorization": api_key}
 payload = {}
 
-while True:
+is_first = True
+
+#!! max items per page = 25 
+    # SO if want a dictionary with everything: iterate from all the pages (using the 'next') 
+
+def get_page_filings(url, is_first):
     response = requests.request("GET", url, headers=headers, data = payload).json()
+
+    if is_first == True:
+        #'count' gives the total number of filings for this specific request: give it on the first iteration
+        print(response['count'])
 
     for item in response['results']:
         lobbying_history[item['filing_uuid']] = [item['registrant']['name'], item['client']['name'], item['expenses'], item['dt_posted'], item['filing_year']]
 
-
-    if response['next'] != None:
-        url = response['next']
-        
-    else:
-        break
+    return response['next']
 
 
-#'count' gives the total number of filings for this specific request 
-#!! max items per page = 25 
-    # SO if want a dictionary with everything: iterate from all the pages (using the 'next') 
+#iterate through all page by jumping from one to the next using the 'next' variable
+while url != None:
+    url = get_page_filings(url, is_first)
+    is_first = False
 
 
-#from response: create a dictionary with registrant name as key and amount reported, date posted, filing year as values
-    #lobbying_history[n] = {'name' = [$xxx, 11-02-2011, 2011]}
+#get the filings of the last page: PROBLEM
+get_page_filings(url, is_first)
 
 
 #convert the dictionary to a pandas df and export as csv 
 df = pd.DataFrame.from_dict(lobbying_history, orient='index')
-df.to_csv('test1.csv')
+df.to_csv(f"{subtopic_edited}1.csv")
 
 print(df)
 
