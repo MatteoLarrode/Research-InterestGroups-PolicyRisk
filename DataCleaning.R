55c643000761249fa2247444e40ecdf4298d3616
MatteoLarrode <matteo.larrode@orange.fr>
2022-11-08 23:26:18 -0500

    data preprocessing template done

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,3 +1,33 @@
 library(tidyverse)
+library(dplyr)
 
-test <- read_csv("test1.csv")
+#import csv's
+deportation_df <- read_csv("deportation1.csv")
+
+#clean dataset (take care of NAs, change date format)
+deportation_df <- deportation_df %>%
+  rename(filing_uuid = 1, 
+         registrant_name = 2,
+         amount_reported = 3,
+         date_posted = 4,
+         filing_year = 5) %>%
+  replace(is.na(.), 0) %>%
+  mutate(date_posted = as.Date(date_posted))
+
+
+#new dataset: collapse by groups
+deportation_groups <- deportation_df %>%
+  group_by(registrant_name)
+
+#variables: how many times lobbied, total & avg amount, start & end years
+deportation_summary <- deportation_groups %>% summarise(
+  times_lobbied = n(),
+  avg_amount = mean(amount_reported),
+  total_amout = sum(amount_reported),
+  start_year = min(filing_year),
+  end_year = max(filing_year)
+)
+
+
+
+
