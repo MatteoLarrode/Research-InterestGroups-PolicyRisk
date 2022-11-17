b5ca65b30210b2f36ec4232e8e536d0cfc279f25
MatteoLarrode <matteo.larrode@orange.fr>
2022-11-17 18:36:15 -0500

    added unique years lobbied variable

--- DataCleaning.R
+++ DataCleaning.R
@@ -8,10 +8,10 @@ deportation_df <- read_csv("deportation1.csv")
 deportation_df <- select(deportation_df, -1)
 deportation_df <- deportation_df %>%
   rename(registrant_name = 1,
-  client_name = 2,
-  amount_reported = 3,
-  date_posted = 4,
-  filing_year = 5) %>%
+         client_name = 2,
+         amount_reported = 3,
+         date_posted = 4,
+         filing_year = 5) %>%
   replace(is.na(.), 0) %>%
   mutate(date_posted = as.Date(date_posted))
 
@@ -21,13 +21,14 @@ deportation_groups <- deportation_df %>%
   group_by(registrant_name)
 
 #variables: how many times lobbied, total & avg amount, start & end years
-deportation_summary <- deportation_groups %>% summarise(
-  times_lobbied = n(),
-  avg_amount = mean(amount_reported),
-  total_amout = sum(amount_reported),
-  start_year = min(filing_year),
-  end_year = max(filing_year)
-)
+deportation_summary <- deportation_groups %>% 
+  summarise(
+    times_lobbied = n(),
+    avg_amount = mean(amount_reported),
+    total_amout = sum(amount_reported),
+    start_year = min(filing_year),
+    end_year = max(filing_year),
+    unique_years = n_distinct(filing_year))
 
 
 
