ae46831d844412e3f24b37b77f72c8bc42d6fb60
MatteoLarrode <matteo.larrode@orange.fr>
2023-03-07 21:54:36 -0500

    api scraping script

--- data_collection/lobbying_scraping.R
+++ data_collection/lobbying_scraping.R
@@ -1,23 +1,36 @@
 library(httr)
-library(dplyr)
 library(jsonlite)
+library(tidyverse)
 
-get_data <- function(subtopic, csvname) {
+#define arguments of function: url, subtopic chosen & name of exit csv file
+
+#url <- "[INSERT URL (use lda website)]"
+#subtopic <- "[INSERT SUBTOPIC]"
+
+csvname <- paste0(subtopic,".csv")
+
+
+get_data <- function(url, subtopic, csvname) {
+
+  #information for GET function
   api_key <- "a11d1ca0026aeefa61592ba43b062da1233c8ad5"
-  
-  url <- paste0("https://lda.senate.gov/api/v1/filings/?filing_dt_posted_after=2011-01-01&filing_dt_posted_before=2021-12-31&filing_specific_lobbying_issues=", subtopic)
-  
   headers <- add_headers("Authorization" = paste0("Token ",api_key))
   payload <- list()
   
-  df <- data.frame(Doubles=double(),
-                    Integers=integer(),
-                    Factors=factor(),
-                    Logicals=logical(),
-                    Characters=character(),
-                    stringsAsFactors=FALSE)
+  #initialize dataframe & naming columns
+  df <- data.frame(filing_id=character(),
+                   filing_year=integer(),
+                   filing_period=character(),
+                   filing_period_display = character(),
+                   expenses=character(),
+                   expenses_method_display = character(),
+                   registrant_name = character(),
+                   registrant_description = character(),
+                   client_name = character(),
+                   client_description = character(),
+                   stringsAsFactors=FALSE)
   
-
+  #iterate through pages (only 25 elements per page)
   while(!is.null(url)){
     get_response <- GET(url, headers = headers)
     api_response <- fromJSON(content(get_response, "text"), flatten = TRUE)
@@ -25,6 +38,7 @@ get_data <- function(subtopic, csvname) {
     print(api_response$count)
     print(api_response$`next`)
     
+    #get info we want from api json response
     if (length(results) > 0) {
       data <- results %>%
         select(filing_uuid, 
@@ -35,12 +49,16 @@ get_data <- function(subtopic, csvname) {
                registrant.name, 
                registrant.description, 
                client.name, 
-               client.general_description)
+               client.general_description) %>%
+        mutate(subtopic = subtopic)
       
+      #append this page to the big dataframe
       df <- rbind(df, data)
+      #attribute "next" url to url variable, breaking the while loop if last page
       url <- api_response$`next`}
     }
-
+  
+  #create csv file! 
   if (length(df) > 0) {
     write.csv(df, paste0(csvname), row.names = FALSE)
     print(paste0("Data for ", subtopic, " fetched and saved to file"))
@@ -49,5 +67,4 @@ get_data <- function(subtopic, csvname) {
   }
 }
 
-subtopic <- "biofuels"
-get_data(subtopic, "test.csv")
+get_data(url, subtopic, csvname)
